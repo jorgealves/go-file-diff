6c2e1998844893a33cf50183d7ab2dfb48c59933
Jorge Alves <jorgeandrealves@gmail.com>
2021-02-19 15:47:18 +0000

    chore: improve main program output

--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/jorgealves/go-file-diff/cmd"
+	"io"
 	"os"
 )
 
 func main() {
 	fileA, fileB := os.Args[1], os.Args[2]
-
 	result := cmd.CompareJsonFiles(fileA, fileB)
-
-	fmt.Println(result)
-
+	var output string
+	if result {
+		output = fmt.Sprintf("Source file: %s\nTarget file: %s\nresult : Files match 👍!\n", fileA, fileB)
+	} else {
+		output = fmt.Sprintf("Source file: %s\nTarget file: %s\nresult : Files do not match 👎!\n", fileA, fileB)
+	}
+	_, _ = io.WriteString(os.Stdout, output)
 }
